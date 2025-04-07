e8ded4155dc8a70c1776b7edd61f0058496f2fbb
sinahamedheidari <sinah65@gmail.com>
2025-04-07 15:30:48 +0330

    Add script and update readme

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# gitlab-export-api
-Shell script to create and download exports from Gitlab using Gitlab API
+# Gitlab Export
+Shell script to create and download project exports from GitLab using the GitLab API.
+
+When exporting a group in GitLab via the UI, it only exports the project structure—not the actual projects. To back up all projects within a group, you need to export each project individually. The Bash script `export_gitlab_projects.sh` helps automate this by generating and downloading project exports easily.
+
+### Prerequisites
+Before running the script, set the following variables:
+
+| Variable | Description|
+| --- | --- |
+|GROUP-ID | The ID of the group whose projects you want to export |
+|PRIVATE-TOKEN | GitLab private token with read/write API access |
+|GITLAB-ADDRESS | Your GitLab instance URL |
+|BACKUP-PATH | Directory path where downloaded files will be saved |
+|PROJECT_LIST_LENGTH | Set to 0 to read all projects in the group |
+|RECORD_PER_PAGE | Number of records to retrieve per page |
+
+### How to use
+```
+git clone https://github.com/sinahamedheidari/gitlab-export
+chmod +x export_gitlab_projects.sh
+## Run the script
+./export_gitlab_projects.sh
+```
+**NOTE**: If you're downloading many projects, it's recommended to run the script inside a terminal multiplexer like screen, tmux, or similar tools to avoid interruptions.
+
+### IMPORTANT NOTE
+You might encounter the error 429 Too Many Requests while running the script. This is due to GitLab’s default API rate limits.
+
+To bypass this:
+
+   1. Go to: https://yourgitlab.com/admin/application_settings/network
+   2. Under the "Import and export rate limits" section, set higher values or 0 (to disable limits temporarily).
+   3. After the export is complete, revert these settings for security best practices.
---
+++ export_gitlab_projects.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+##Constants
+GROUP_ID=GILAB-GROUP-ID
+PRIVATE_TOKEN="PRIVATE-TOKEN"
+GITLAB_ADDRESS="GITLAB-URL"
+PROJECT_LIST_LENGTH=0 #Default
+RECORD_PER_PAGE=100 #Default
+EXPORT_DIR_PATH="BACKUP-PATH"
+
+## Get Total project count of a group
+TOTAL_PROJECT_COUNT=$(curl --silent --head --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "$GITLAB_ADDRESS/api/v4/groups/$GROUP_ID/projects?include_subgroups=true"|grep "X-Total:"|awk '{print $2}'|tr -d '\r')
+echo "Total project count: $TOTAL_PROJECT_COUNT"
+
+##Fetch Project List
+echo "Feching Project List..."
+## PROJECT_LIST Items: ("PATH_WITH_NAMESPACES,PROJECT_ID,WEB_URL")
+PROJECT_LIST=()
+PAGE_NO=1
+while [[ $PROJECT_LIST_LENGTH < $TOTAL_PROJECT_COUNT ]];
+do
+    PROJECT_LIST+=($(curl --silent --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" "$GITLAB_ADDRESS/api/v4/groups/$GROUP_ID/projects?include_subgroups=true&per_page=$RECORD_PER_PAGE&page=$PAGE_NO"|jq '.[]|"\(.path_with_namespace),\(.id),\(.web_url)"'|tr -d '"'))
+    PROJECT_LIST_LENGTH="${#PROJECT_LIST[@]}"
+    ((PAGE_NO++))
+    echo "Fetched $PROJECT_LIST_LENGTH"
+    sleep 1
+done
+
+##Run export command and download projects
+for item in "${PROJECT_LIST[@]}"; do
+    # Split the item into separate variables
+    IFS=',' read -r P_PATH P_ID P_WEB_URL <<< "$item"
+
+    # Send export request
+    curl --silent --request POST "$GITLAB_ADDRESS/api/v4/projects/$P_ID/export" --header "PRIVATE-TOKEN: $PRIVATE_TOKEN"
+    echo ""
+
+    # Check wheteher export is finished or not
+    EXPORT_STATUS=""
+    while [[ "$EXPORT_STATUS" != 'finished' ]];
+    do
+        EXPORT_STATUS=$(curl --silent --request GET "$GITLAB_ADDRESS/api/v4/projects/$P_ID/export" --header "PRIVATE-TOKEN: $PRIVATE_TOKEN"|jq .export_status|tr -d '\r'|tr -d '"')
+        echo $EXPORT_STATUS
+        sleep 1
+    done
+
+    # Download export
+    EXPORT_FILE_NAME="${P_PATH//\//_}"
+    echo $P_ID
+    curl --request GET "$GITLAB_ADDRESS/api/v4/projects/$P_ID/export/download" --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" -o $EXPORT_DIR_PATH/$EXPORT_FILE_NAME.tar.gz
+    sleep 30
+
+done
